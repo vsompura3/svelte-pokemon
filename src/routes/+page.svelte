5c6c8083d12e084b1fbfb93a21fa6fb8911fe49f
Vaibhav Sompura <42895093+vsompura3@users.noreply.github.com>
2023-03-18 02:02:09 +0530

    added tailwindcss and postcss to the project

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex justify-center items-center flex-col py-6 gap-2">
+  <h1 class="text-lg">Welcome to SvelteKit</h1>
+  <p class="text-4xl">
+    Learning <span class="text-[#fc2606] font-black">Svelte</span>
+  </p>
+</div>
+
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.gray.100);
+  }
+</style>
