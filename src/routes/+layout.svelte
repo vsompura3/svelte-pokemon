be030cecd22c522578fc22f76d79198975cce217
Vaibhav Sompura <42895093+vsompura3@users.noreply.github.com>
2023-03-21 21:18:37 +0530

    fixed pokemon fetching, pokemon gets fetched on component load

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
   import Nav from "../components/nav.svelte";
 </script>
 
-<div class="p-8 max-w-6xl mx-auto">
+<div class="p-8 max-w-7xl w-11/12 mx-auto">
   <Nav/>
   <slot />
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,12 @@
 <script>
   import PokemanCard from '../components/pokemanCard.svelte';
-  import { pokemon } from '../stores/pokestore';
+  export let data;
+  const {pokemon} = data;
 
   let searchTerm = ''
   let filteredPokemon = []
 
   $: {
-    console.log(searchTerm)
     if (searchTerm) {
       filteredPokemon = $pokemon.filter(pokeman => pokeman.name.toLowerCase().includes(searchTerm.toLowerCase()))
     } else {
@@ -18,19 +18,21 @@
 <svelte:head>
   <title>Svelte Kit Pokedex</title>
 </svelte:head>
-<h1 class="text-4xl text-center my-8 uppercase">Svelte Kit Pokedex</h1>
+<h1 class="text-4xl font-extralight text-center my-8 uppercase">Svelte Kit <a href="/" class="text-red-500 font-semibold">Pokedex</a></h1>
 
 <label class="hidden" for="poke-search">Search for a poekmon:</label>
-<input class="w-full rounded-md text-lg p-4 border-2 border-gray-200" type="text" name="poke-search" id="poke-search" placeholder="Search Pokemon" bind:value={searchTerm}>
+<input class="w-full rounded-md text-lg p-4 border-2 border-gray-200 outline-red-500 outline-offset-4" type="text" name="poke-search" id="poke-search" placeholder="Search Pokemon" bind:value={searchTerm}>
 
-<div class="grid gap-4 mt-4 grid-cols-1 md:grid-cols-2">
+<div class="grid gap-4 mt-4 grid-cols-1 sm:grid-cols-2">
   {#each filteredPokemon as pokeman}
-    <PokemanCard pokeman={pokeman}/>
+    <PokemanCard pokeman={pokeman} />
   {/each}
 </div>
 
 <style lang="postcss">
+  /*
   :global(html) {
     background-color: theme(colors.gray.100);
-  }
+  } 
+  */
 </style>
